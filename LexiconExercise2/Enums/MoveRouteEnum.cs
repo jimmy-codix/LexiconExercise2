ed5a7abafb3b081d9b60779596402c0bf6e0eb13
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-05-29 13:19:36 +0200

    Added Enum to select a route in TaskMovies (single or group).

---
+++ LexiconExercise2/Enums/MoveRouteEnum.cs
@@ -0,0 +1,15 @@
+﻿using LexiconExercise2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise2.Enums
+{
+    enum MovieRoute
+    {
+        Single,
+        Group
+    }
+}
