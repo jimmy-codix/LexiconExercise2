a4c547e3bd33b99a24168f175471428720150bdc
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-05-29 13:17:55 +0200

    Added TaskSplit for stringsplitting.

---
+++ LexiconExercise2/Tasks/TaskSplit.cs
@@ -0,0 +1,26 @@
+﻿using LexiconExercise2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise2.Tasks
+{
+    internal class TaskSplit : ITask
+    {
+        
+        public void Work(IInputOutputManager io)
+        {
+            io.Clear();
+            io.WriteLine("**String splitter**");
+            io.WriteLine("Write a sentence with at least 3 words. I will then display the 3rd word:");
+            string test = io.ReadLine();
+            var strings = test.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            if (strings.Length < 3)
+                io.WriteLine("There are not 3 words in your sentence.");
+            else
+                io.WriteLine($"Your third word was: {strings[2]}");
+        }
+    }
+}
