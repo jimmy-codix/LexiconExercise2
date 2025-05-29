ee0b56c31315e0e00f8e44186af141b9f534e0ef
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-05-29 13:16:10 +0200

    Use of Interface for IO.

--- LexiconExercise2/MainMenu.cs
+++ LexiconExercise2/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LexiconExercise2.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,12 @@ namespace LexiconExercise2
 {
     internal static class MainMenu
     {
-        public static void Print(InputOutputManager io)
+        public static void Print(IInputOutputManager io)
         {
             io.Clear();
             io.WriteLine("**Welcome to the main menu**");
-            io.WriteLine("1. Movies, calculate price for one person.");
-            io.WriteLine("2. Movies, calculate price for a group.");
+            io.WriteLine("1. Movies, calculate price (Single).");
+            io.WriteLine("2. Movies, calculate price (Group).");
             io.WriteLine("3. Repeat 10 times.");
             io.WriteLine("4. String splitter.");
             io.WriteLine("0. Exit this program.");
